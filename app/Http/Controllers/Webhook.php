76a0379a7dd8a5a95b7dc0bfa7ebe5e11f6302d7
zidane-sc <zidankren02@gmail.com>
2020-12-28 01:40:52 +0700

    Fix Error

--- app/Http/Controllers/Webhook.php
+++ app/Http/Controllers/Webhook.php
@@ -1,7 +1,8 @@
 <?php
- 
+
+
 namespace App\Http\Controllers;
- 
+
 use App\Gateway\EventLogGateway;
 use App\Gateway\QuestionGateway;
 use App\Gateway\UserGateway;
@@ -16,7 +17,7 @@ use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
 use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
- 
+
 class Webhook extends Controller
 {
     /**
@@ -51,8 +52,8 @@ class Webhook extends Controller
      * @var array
      */
     private $user;
- 
- 
+
+
     public function __construct(
         Request $request,
         Response $response,
@@ -60,47 +61,48 @@ class Webhook extends Controller
         EventLogGateway $logGateway,
         UserGateway $userGateway,
         QuestionGateway $questionGateway
-    ) {
+    )
+    {
         $this->request = $request;
         $this->response = $response;
         $this->logger = $logger;
         $this->logGateway = $logGateway;
         $this->userGateway = $userGateway;
         $this->questionGateway = $questionGateway;
- 
+
         // create bot object
         $httpClient = new CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-        $this->bot  = new LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+        $this->bot = new LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
     }
 
     public function __invoke()
     {
         // get request
         $body = $this->request->all();
-    
+
         // debuging data
         $this->logger->debug('Body', $body);
-    
+
         // save log
         $signature = $this->request->server('HTTP_X_LINE_SIGNATURE') ?: '-';
         $this->logGateway->saveLog($signature, json_encode($body, true));
-    
+
         return $this->handleEvents();
     }
 
     private function handleEvents()
     {
         $data = $this->request->all();
-    
+
         if(is_array($data['events'])){
             foreach ($data['events'] as $event)
             {
                 // skip group and room event
-                if(!isset($event['source']['userId'])) continue;
-    
+                if(! isset($event['source']['userId'])) continue;
+
                 // get user data from database
                 $this->user = $this->userGateway->getUser($event['source']['userId']);
-    
+
                 // if user not registered
                 if(!$this->user) $this->followCallback($event);
                 else {
@@ -117,8 +119,8 @@ class Webhook extends Controller
                 }
             }
         }
-    
-    
+
+
         $this->response->setContent("No events found!");
         $this->response->setStatusCode(200);
         return $this->response;
@@ -130,29 +132,29 @@ class Webhook extends Controller
         if ($res->isSucceeded())
         {
             $profile = $res->getJSONDecodedBody();
-    
+
             // create welcome message
             $message  = "Salam kenal, " . $profile['displayName'] . "!\n";
             $message .= "Silakan kirim pesan \"MULAI\" untuk memulai kuis Tebak Kode.";
             $textMessageBuilder = new TextMessageBuilder($message);
-    
+
             // create sticker message
             $stickerMessageBuilder = new StickerMessageBuilder(1, 3);
-    
+
             // merge all message
             $multiMessageBuilder = new MultiMessageBuilder();
             $multiMessageBuilder->add($textMessageBuilder);
             $multiMessageBuilder->add($stickerMessageBuilder);
-    
+
             // send reply message
             $this->bot->replyMessage($event['replyToken'], $multiMessageBuilder);
-    
+
             // save user data
             $this->userGateway->saveUser(
                 $profile['userId'],
                 $profile['displayName']
             );
-    
+
         }
     }
 
@@ -174,7 +176,7 @@ class Webhook extends Controller
                 $textMessageBuilder = new TextMessageBuilder($message);
                 $this->bot->replyMessage($event['replyToken'], $textMessageBuilder);
             }
-    
+
             // if user already begin test
         } else {
             $this->checkAnswer($userMessage, $event['replyToken']);
@@ -185,16 +187,16 @@ class Webhook extends Controller
     {
         // create sticker message
         $stickerMessageBuilder = new StickerMessageBuilder(1, 106);
-    
+
         // create text message
         $message = 'Silakan kirim pesan "MULAI" untuk memulai kuis.';
         $textMessageBuilder = new TextMessageBuilder($message);
-    
+
         // merge all message
         $multiMessageBuilder = new MultiMessageBuilder();
         $multiMessageBuilder->add($stickerMessageBuilder);
         $multiMessageBuilder->add($textMessageBuilder);
-    
+
         // send message
         $this->bot->replyMessage($event['replyToken'], $multiMessageBuilder);
     }
@@ -203,19 +205,19 @@ class Webhook extends Controller
     {
         // get question from database
         $question = $this->questionGateway->getQuestion($questionNum);
-    
+
         // prepare answer options
         for($opsi = "a"; $opsi <= "d"; $opsi++) {
             if(!empty($question['option_'.$opsi]))
                 $options[] = new MessageTemplateActionBuilder($question['option_'.$opsi], $question['option_'.$opsi]);
         }
-    
+
         // prepare button template
         $buttonTemplate = new ButtonTemplateBuilder($question['number']."/10", $question['text'], $question['image'], $options);
-    
+
         // build message
         $messageBuilder = new TemplateMessageBuilder("Gunakan mobile app untuk melihat soal", $buttonTemplate);
-    
+
         // send message
         $response = $this->bot->replyMessage($replyToken, $messageBuilder);
     }
@@ -227,12 +229,12 @@ class Webhook extends Controller
             $this->user['score']++;
             $this->userGateway->setScore($this->user['user_id'], $this->user['score']);
         }
-    
+
         if($this->user['number'] < 10)
         {
             // update number progress
             $this->userGateway->setUserProgress($this->user['user_id'], $this->user['number'] + 1);
-    
+
             // send next question
             $this->sendQuestion($replyToken, $this->user['number'] + 1);
         }
@@ -240,23 +242,23 @@ class Webhook extends Controller
             // create user score message
             $message = 'Skormu '. $this->user['score'];
             $textMessageBuilder1 = new TextMessageBuilder($message);
-    
+
             // create sticker message
             $stickerId = ($this->user['score'] < 8) ? 100 : 114;
             $stickerMessageBuilder = new StickerMessageBuilder(1, $stickerId);
-    
+
             // create play again message
             $message = ($this->user['score'] < 8) ?
                 'Wkwkwk! Nyerah? Ketik "MULAI" untuk bermain lagi!':
                 'Great! Mantap bro! Ketik "MULAI" untuk bermain lagi!';
             $textMessageBuilder2 = new TextMessageBuilder($message);
-    
+
             // merge all message
             $multiMessageBuilder = new MultiMessageBuilder();
             $multiMessageBuilder->add($textMessageBuilder1);
             $multiMessageBuilder->add($stickerMessageBuilder);
             $multiMessageBuilder->add($textMessageBuilder2);
-    
+
             // send reply message
             $this->bot->replyMessage($replyToken, $multiMessageBuilder);
             $this->userGateway->setUserProgress($this->user['user_id'], 0);
